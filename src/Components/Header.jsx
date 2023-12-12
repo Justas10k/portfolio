3596b87294afac7454ab411b5ae74e4ec1ca7770
Justas Stankevičius <justas.stankevicius@stud.kitm.lt>
2023-12-12 19:48:59 +0200

    pdf upt

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import "../Styles/Header.css";
 import HandEmote from "../img/HandEmote.png";
 import flag from "../img/flag.png";
 import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
-import Resume from "../Pdf/Justas_resume.pdf";
+import Resume from "../Pdf/Justas_Stankevi_resume.pdf";
 
 const Header = () => {
   const skillsList = [
